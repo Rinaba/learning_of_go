a7726530c56e7e402a82ee781c7ab78bb7789683
稲葉遼一 <ryoichiinaba@Erio.local>
2024-05-08 20:49:14 +0900

    change fileencoding utf-8

--- concurrency_in_go/1_2_1.go
+++ concurrency_in_go/1_2_1.go
@@ -10,12 +10,12 @@ func main () {
 	go func() {
 		data++
 	} ()
-	//$B0J2<$N(BSLEEP$B=hM}$rM-8z$K$9$k$H(Bif$BJ8$NI>2AA0$K(Bdata$B$r2C;;$9$k(B
-	//$B4|BTCM$O(B "$BI=<($J$7(B"
+	//以下のSLEEP処理を有効にするとif文の評価前にdataを加算する
+	//期待値は "表示なし"
 	//time.Sleep(1*time.Second)
 	if data == 0 {
-		//$B0J2<$N(BSLEEP$B=hM}$rM-8z$K$9$k$H(Bif$BJ8I>2A8e$K(Bdata$B$r2C;;$9$k(B
-		//$B4|BTCM$O(B "the value is 1."
+		//以下のSLEEP処理を有効にするとif文評価後にdataを加算する
+		//期待値は "the value is 1."
 		//time.Sleep(1*time.Second)
 		fmt.Printf("the value is %v.\n", data)
 	}

74ca15724f7d8602d7a394ed47e1e96ab6a08d82
稲葉遼一 <ryoichiinaba@Erio.local>
2024-05-08 21:10:46 +0900

    コメントをより正確な表現に修正した

--- concurrency_in_go/1_2_1.go
+++ concurrency_in_go/1_2_1.go
@@ -10,11 +10,11 @@ func main () {
 	go func() {
 		data++
 	} ()
-	//以下のSLEEP処理を有効にするとif文の評価前にdataを加算する
+	//以下のSLEEP処理を有効にするとif文の評価前にdataを加算する可能性が高くなる
 	//期待値は "表示なし"
 	//time.Sleep(1*time.Second)
 	if data == 0 {
-		//以下のSLEEP処理を有効にするとif文評価後にdataを加算する
+		//以下のSLEEP処理を有効にするとif文評価後にdataを加算する可能性が高くなる
 		//期待値は "the value is 1."
 		//time.Sleep(1*time.Second)
 		fmt.Printf("the value is %v.\n", data)
